e46caa6667e9beff0d7a99a0ecbcf577ef306fd6
Fanaina Alves <fanaiina.allves@gmail.com>
2024-03-05 15:21:23 -0300

    Migração de Lombok para Getter e Setter tradicional

--- src/main/java/br/com/tddjava/tddjava/modules/courses/entities/Course.java
+++ src/main/java/br/com/tddjava/tddjava/modules/courses/entities/Course.java
@@ -1,19 +1,16 @@
 package br.com.tddjava.tddjava.modules.courses.entities;
 
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
 @Table(name = "courses")
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Course {
 
     @Id
@@ -23,4 +20,45 @@ public class Course {
     private String name;
     private int workload;
 
+    public Course(UUID id, String description, String name, int workload) {
+        this.id = id;
+        this.description = description;
+        this.name = name;
+        this.workload = workload;
+    }
+
+    public Course() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWorkload() {
+        return workload;
+    }
+
+    public void setWorkload(int workload) {
+        this.workload = workload;
+    }
 }
